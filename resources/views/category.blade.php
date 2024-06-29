405722d5d83a073f656de811b1fe6c856762f5e5
kacioussama0 <kacioussama0@gmail.com>
2024-06-29 19:31:45 +0100

    update-status

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -11,7 +11,7 @@
         <h1 class="uk-h1 uk-text-center">{{$category->name()}}</h1>
         <p class="uk-text-lead uk-text-center">{{$category->description()}}</p>
         <div class="uk-child-width-1-3@m uk-grid-match uk-text-center uk-margin-medium-top uk-grid uk-grid-stack" data-uk-grid="">
-            @foreach($category->spaces as $space)
+            @foreach($category->spaces()->orderBy("status_id")->get() as $space)
             <div class="uk-first-column">
                     <div class="uk-card uk-card-default uk-box-shadow-medium uk-card-hover uk-card-body uk-inline border-radius-large border-xlight">
                         <a class="uk-position-cover" href="{{url($locale . '/spaces/' . $space->slug())}}" ></a>
